f5e66d32bb04d27fee4ccdfacf7506ee897ef7f9
anajuliamoreno172-hue <anajuliamoreno172@gmail.com>
2025-09-10 14:08:02 -0300

    Add files via upload

---
+++ Modulo 1/Exercicios/try atv.py
@@ -0,0 +1,10 @@
+senha = input('digite um indice')
+num1 = int(input("digite o primeiro numero: "))
+num2 = int(input("digite o segundo numero: "))
+
+try:
+  print(num1/num2)
+
+except:
+  print("voce digitou um zero nao faça isso")
+  
